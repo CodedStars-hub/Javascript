f03c6bc2d1c6e16de1fa87aa275938e183d07e7c
CodedStars-hub <starsxcodes@gmail.com>
2026-09-06 01:23:00 +0530

    Js basics

---
+++ basics/index.js
@@ -0,0 +1,11 @@
+let firstName = "Harshita";
+let age = 20;
+let student = true;
+
+console.log("Hello " + firstname);
+console.log("You are ", age, " years old!");
+console.log("Enrolled: ", student);
+
+document.getElementById("p1").innerHTML = "hello " + firstname;
+document.getElementById("p2").innerHTML = "Your age is: " + age + " years old.";
+document.getElementById("p3").innerHTML = "Enrolled: " + firstname;
